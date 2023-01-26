d20d35c38818062d7c2cecbe663882ca3b49e556
motea927 <motea927@yahoo.com.tw>
2023-01-27 00:07:12 +0800

    feat: Init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app">This is our plugin components</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.app {
+  color: yellow;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import App from './App.vue'
+
+const isDev = import.meta.env.MODE === 'development'
+if (isDev) {
+  const { createApp } = await import('vue')
+  const PlayGroundApp = (await import('../playground/App.vue')).default
+  await import('../playground/assets/main.css')
+  createApp(PlayGroundApp).mount('#app')
+}
+
+export { App }
