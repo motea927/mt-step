7760a7e2afdfc5899dcc81fe87574f46ea408fe7
motea927 <motea927@yahoo.com.tw>
2023-01-28 17:52:51 +0800

    build: add auto generate type plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      include: ['src/**'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
