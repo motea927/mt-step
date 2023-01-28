696522abaa851d67a4f1d1749903303dfe62523d
motea927 <motea927@yahoo.com.tw>
2023-01-28 22:55:41 +0800

    docs: add single highlight element demo

---
+++ docs/components/base/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="base-button">
+    <slot> show demo </slot>
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.base-button {
+  background-color: #00b9e1;
+  color: #fff;
+  border-radius: 4px;
+  padding: 8px;
+}
+</style>
