18ec6402c6feeff9e5c5ea70a5e53ae9ebb82ccc
motea927 <motea927@yahoo.com.tw>
2023-01-28 19:01:41 +0800

    docs: add usage example

---
+++ docs/components/content/guide/UsageExample.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="usage-example">
+    <button class="usage-example__button" @click="showStep = true">
+      show demo
+    </button>
+
+    <MtStep v-if="showStep">
+      <MtStepItem
+        :stepItem="[
+          {
+            selector: '#get-started',
+            hint: { text: 'This is a title' },
+          },
+        ]"
+      />
+      <MtStepItem
+        :stepItem="[
+          {
+            selector: '.usage-example__button',
+            hint: { text: 'This is a button' },
+          },
+        ]"
+      />
+    </MtStep>
+  </div>
+</template>
+
+<script setup lang="ts">
+import 'mt-step/css'
+import { MtStep, MtStepItem } from 'mt-step'
+
+import { ref } from 'vue'
+
+const showStep = ref(false)
+</script>
+
+<style lang="scss">
+.usage-example {
+  &__button {
+    background-color: #00b9e1;
+    color: #fff;
+    border-radius: 4px;
+    padding: 8px;
+  }
+}
+</style>
